19d2e5ce802fe0c67371549f434c66eef2745adb
Cole Flannery <jaidencoleflannery@gmail.com>
2026-02-23 22:48:33 -0600

    tests | basic tests for configurations.

--- testing/Tools/Commands/CommandServiceTests.cs
+++ testing/Tools/Commands/CommandServiceTests.cs
@@ -1,31 +1,41 @@
 using Xunit;
 using chat.net.Commands;
+using chat.net.Configurations;
 
 namespace chat.net.Testing.Commands;
 
-public class CommandValidationServiceTests {
-
-    // REPLACE THESE
-
-    [Fact]
-    public void ValidateCommandsSetProviderReturnsConfigCommand() {
-        string[] args = { "--config", "--set-provider", "google" };
-        Config response = (Config)CommandValidationService.ValidateCommands(args);
-
-        Assert.IsType<Config>(response);
-        Assert.IsType<ConfigActionRequiresArgument>(response.ActionArgument);
-        Assert.Equal(ConfigActionRequiresArgument.SetProvider, response.ActionArgument);
-        Assert.Equal("google", response.Value);
-    }
+public class CommandServiceTests {
 
     [Theory]
-    [InlineData("--config", "--set-provider", "random")]
-    [InlineData("--config", "--set-provider")]
-    [InlineData("--config", "")]
-    public void ValidateCommandsSetProviderBadReturnsNull(params string[] args) {
-        var response = CommandValidationService.ValidateCommands(args);
+    [InlineData("gemini")]
+    [InlineData("google")]
+    [InlineData("uncle rodrick's super sick local llm cluster")]    
+    public void ExecuteConfigSetModelChangesConfig(string val) {
+        Config command = new Config(ActionArgument: ConfigActionRequiresArgument.SetModel, Value: val);
+        CommandService.Execute(command);
 
-        Assert.Null(response);
+        Configuration? config = ConfigurationService.GetConfig();
+        Assert.NotNull(config);
+        Assert.NotNull(config.Model);
+        Assert.Equal(command.Value, config.Model);
     }
 
+    [Fact] 
+    public void ExecuteConfigSetModelBadDoesntChangeConfig() {
+        Config command1 = new Config(ActionArgument: ConfigActionRequiresArgument.SetModel, Value: "gemini");
+        CommandService.Execute(command1);
+
+        Configuration? config = ConfigurationService.GetConfig();
+        Assert.NotNull(config);
+        Assert.NotNull(config.Model);
+        Assert.Equal(command1.Value, config.Model);
+
+        Config command2 = new Config(ActionArgument: ConfigActionRequiresArgument.SetModel, Value: "");
+        CommandService.Execute(command2);
+
+        config = ConfigurationService.GetConfig();
+        Assert.NotNull(config);
+        Assert.NotNull(config.Model);
+        Assert.Equal(command1.Value, config.Model);
+    }
 }
